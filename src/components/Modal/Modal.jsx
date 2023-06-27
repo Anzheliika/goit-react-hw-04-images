e9407bb6cdfe42289976e30b6ce35ec27cdad975
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-06-27 20:58:32 +0300

    refactored all the code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
@@ -7,11 +6,6 @@ import { Overlay, ModalWindow } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
 
 export default function Modal({ onClose, children }) {
-  // static propTypes = {
-  //   children: PropTypes.node.isRequired,
-  //   onClose: PropTypes.func.isRequired,
-  // };
-
   useEffect(() => {
     const handleKeyDown = e => {
       if (e.code === 'Escape') onClose();
@@ -37,38 +31,8 @@ export default function Modal({ onClose, children }) {
   );
 }
 
-// export default class Modal extends Component {
-//   static propTypes = {
-//     children: PropTypes.node.isRequired,
-//     onClose: PropTypes.func.isRequired,
-//   };
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleBackdropClick = event => {
-//     if (event.currentTarget === event.target) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     return createPortal(
-//       <Overlay onClick={this.handleBackdropClick}>
-//         <ModalWindow>{this.props.children} </ModalWindow>
-//       </Overlay>,
-//       modalRoot
-//     );
-//   }
-// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import toast, { Toaster } from 'react-hot-toast';
 import {
@@ -9,44 +10,43 @@ import {
   SearchFormInput,
 } from './Searchbar.styled';
 
-export default class SearchBar extends React.Component {
-  static propTypes = { onSubmit: PropTypes.func.isRequired };
+export default function SearchBar({ onSubmit }) {
+  const [pictureName, setPictureName] = useState('');
 
-  state = {
-    pictureName: '',
+  const handleSearchChange = e => {
+    setPictureName(e.currentTarget.value.toLowerCase());
   };
 
-  handleSearchChange = e => {
-    this.setState({ pictureName: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.pictureName.trim() === '') {
+    if (pictureName.trim() === '') {
       return toast.error('Enter a search query');
     }
-    this.props.onSubmit(this.state.pictureName);
+    onSubmit(pictureName);
   };
 
-  render() {
-    return (
-      <SearchbarHeader>
-        <Toaster />
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchFormButton type="submit">
-            <SearchFormButtonLabel>Search</SearchFormButtonLabel>
-          </SearchFormButton>
+  return (
+    <SearchbarHeader>
+      <Toaster />
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormButton type="submit">
+          <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+        </SearchFormButton>
 
-          <SearchFormInput
-            value={this.state.pictureName}
-            onChange={this.handleSearchChange}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </SearchbarHeader>
-    );
-  }
+        <SearchFormInput
+          value={pictureName}
+          onChange={handleSearchChange}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </SearchbarHeader>
+  );
 }
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
